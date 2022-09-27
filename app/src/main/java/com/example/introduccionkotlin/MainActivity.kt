df92750b9ba50a1c9fe98fee4f622b6918f548f6
Marcelo Moll <mollmarcelomoll@gmail.com>
2022-09-27 19:50:27 -0300

    27-09-2022 Herencia

--- app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
+++ app/src/main/java/com/example/introduccionkotlin/MainActivity.kt
@@ -8,14 +8,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var a: Persona = Persona(name = "Marcelo", passport = "Argentino")
+
+        var a: Persona = Persona(name = "Marcelo", passport = "AQQW7874")
         var b: Persona = Persona()
+        println(a.alive)
         println(a.name)
         println(a.passport)
 
         b.Persona()
+        b.name = "Marc"
+        println(b.alive)
         println(b.name)
         println(b.passport)
+
+        var messi: Athlete = Athlete(name = "Messi", passport = "ERT4848", sport = "Futbol")
+
+        println(messi.alive)
+        println(messi.name)
+        println(messi.passport)
+        println(messi.sport)
 
        var c: Pokemon = Pokemon()
     }
--- app/src/main/java/com/example/introduccionkotlin/Persona.kt
+++ app/src/main/java/com/example/introduccionkotlin/Persona.kt
@@ -1,9 +1,15 @@
 package com.example.introduccionkotlin
 
-class Persona(var name: String ="Anonimo", var passport: String? = null) {
+open class Persona (var name: String = "Anonimo", var passport: String? = null) {
+    var alive : Boolean = true
 
-    fun Persona(){
-        name = "Marcelo"
-        passport = "Canada"
+    fun Persona () {
+        name = "Marc"
+        passport = "BSE45454"
+    }
+    fun die () {
+        alive = false
     }
 }
+
+class Athlete (name: String, passport: String?, var sport: String): Persona (name, passport)
