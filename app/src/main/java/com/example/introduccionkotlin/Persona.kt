b0040035b4128e40452696ee5317de7e38f79602
Marcelo Moll <mollmarcelomoll@gmail.com>
2022-09-28 21:16:39 -0300

    28-09-2022 Herencia

--- app/src/main/java/com/example/introduccionkotlin/Persona.kt
+++ app/src/main/java/com/example/introduccionkotlin/Persona.kt
@@ -11,5 +11,7 @@ open class Persona (var name: String = "Anonimo", var passport: String? = null) 
         alive = false
     }
 }
-
+class Datos (var age: Int, var altura: Int, var peso: Int) : Persona (name, passport)
 class Athlete (name: String, passport: String?, var sport: String): Persona (name, passport)
+
+//class Datee (var age: Int, var height: Int): Persona (name, passport)
